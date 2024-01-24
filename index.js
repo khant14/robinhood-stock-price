d064393d07ead32816c1794336bd3fe1204b3084
Aung Khant <khant@Aungs-MacBook-Pro.local>
2024-01-24 01:01:42 -0600

    catch errors

--- index.js
+++ index.js
@@ -18,22 +18,25 @@ let searchTicker;
   await page.setViewport({ width: 1080, height: 1024 });
 
   app.get('/:ticker', async (req, res) => {
-    // If user request a different ticker, navigate to page.
-    if (searchTicker !== req.params.ticker) {
-      searchTicker = req.params.ticker;
+    try {
+      // If user request a different ticker, navigate to page.
+      if (searchTicker !== req.params.ticker) {
+        searchTicker = req.params.ticker;
 
-      // Navigate the page to a URL
-      await page.goto(`https://robinhood.com/us/en/stocks/${searchTicker}/`);
+        // Navigate the page to a URL
+        await page.goto(`https://robinhood.com/us/en/stocks/${searchTicker}/`);
 
-      // Wait for page load
-      await page.waitForSelector('#sdp-market-price');
+        // Wait for page load
+        await page.waitForSelector('#sdp-market-price');
+      }
+
+      // Locate the price. TODO: Find better way to get price
+      const element = await page.$(".css-whh5e5");
+      let value = await page.evaluate(el => el.textContent, element)
+      res.send(value);
+    } catch (e) {
+      res.send('Error');
     }
-
-    // Locate the price. TODO: Find better way to get price
-    const element = await page.$(".css-whh5e5");
-    let value = await page.evaluate(el => el.textContent, element)
-
-    res.send(value);
   });
 
   app.listen(process.env.PORT, () =>

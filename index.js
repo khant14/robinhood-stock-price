9703143b39fd940b98cd71202617af139d063f67
Aung Khant <khant@Aungs-MBP.lan>
2024-01-26 02:23:58 -0600

    multi tabs

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const puppeteer = require('puppeteer');
 const express = require('express');
 const app = express();
 let browser;
-let page;
-let searchTicker;
+let pageMap = new Map();
 const PORT = process.env.PORT || 8001;
 const BLOCKED_RESOURCES = ['image', 'stylesheet', 'ping', 'font'];
 
@@ -14,33 +13,35 @@ const BLOCKED_RESOURCES = ['image', 'stylesheet', 'ping', 'font'];
       '--no-sandbox', '--disable-setuid-sandbox'
     ]
   });
-  page = await browser.newPage();
-  await page.setRequestInterception(true);
-  page.on("request", (request) => {
-    if (!request.url().includes('robinhood') || BLOCKED_RESOURCES.includes(request.resourceType())) {
-      request.abort();
-    } else {
-      request.continue();
-    }
-  });
 
-  // Set screen size
-  await page.setViewport({ width: 100, height: 100 });
+  const createPage = async (ticker) => {
+    const page = await browser.newPage();
+    await page.setRequestInterception(true);
+
+    // Block content for loading faster
+    page.on("request", (request) => {
+      if (!request.url().includes('robinhood') || BLOCKED_RESOURCES.includes(request.resourceType())) {
+        request.abort();
+      } else {
+        request.continue();
+      }
+    });
+    // Set screen size
+    await page.setViewport({ width: 100, height: 100 });
+    // Navigate the page to a URL
+    await page.goto(`https://robinhood.com/us/en/stocks/${ticker}/`);
+    // Wait for page load
+    await page.waitForSelector('#sdp-market-price');
+
+    return page;
+  }
 
   app.get('/:ticker', async (req, res) => {
     try {
-      // If user request a different ticker, navigate to page.
-      if (searchTicker !== req.params.ticker) {
-        searchTicker = req.params.ticker;
+      const ticker = req.params.ticker;
+      const page = pageMap.has(ticker) ? pageMap.get(ticker) : await createPage(ticker);
 
-        // Navigate the page to a URL
-        await page.goto(`https://robinhood.com/us/en/stocks/${searchTicker}/`);
-
-        // Wait for page load
-        await page.waitForSelector('#sdp-market-price');
-      }
-
-      // Locate the price. TODO: Find better way to get price
+      // Locate the price.
       const element = await page.$(".css-whh5e5");
       let value = await page.evaluate(el => el.textContent, element)
       res.send(value);

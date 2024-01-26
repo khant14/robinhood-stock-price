9dfccb0985e3be07bc9f2d94f9ea75ad175948f4
Aung Khant <khant@Aungs-MBP.lan>
2024-01-26 11:18:57 -0600

    pre cache response

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 let browser;
 let pageMap = new Map();
+let priceMap = new Map();
 const PORT = process.env.PORT || 8001;
 const BLOCKED_RESOURCES = ['image', 'stylesheet', 'ping', 'font'];
 
@@ -19,12 +20,22 @@ const BLOCKED_RESOURCES = ['image', 'stylesheet', 'ping', 'font'];
     await page.setRequestInterception(true);
 
     // Block content for loading faster
-    page.on("request", (request) => {
+    page.on("request", request => {
       if (!request.url().includes('robinhood') || BLOCKED_RESOURCES.includes(request.resourceType())) {
         request.abort();
       } else {
         request.continue();
       }
+    });
+    // Listen for stock price change
+    page.on('response', async (response) => {
+      try {
+        if (response.url().includes("marketdata/quotes")) {
+          const stockQuotes = await response.json();
+          const symbol = stockQuotes.results[0].symbol;
+          priceMap.set(symbol, stockQuotes.results[0]);
+        }
+      } catch (e) { }
     });
     // Set screen size
     await page.setViewport({ width: 100, height: 100 });
@@ -34,19 +45,15 @@ const BLOCKED_RESOURCES = ['image', 'stylesheet', 'ping', 'font'];
     await page.waitForSelector('#sdp-market-price');
     // Save page tab for future
     pageMap.set(ticker, page);
-
-    return page;
   }
 
   app.get('/:ticker', async (req, res) => {
     try {
       const ticker = req.params.ticker;
-      const page = pageMap.has(ticker) ? pageMap.get(ticker) : await createPage(ticker);
+      if (!pageMap.has(ticker)) await createPage(ticker);
+      const stockQuote = priceMap.get(ticker);
 
-      // Locate the price.
-      const element = await page.$(".css-whh5e5");
-      let value = await page.evaluate(el => el.textContent, element)
-      res.send(value);
+      res.send(stockQuote);
     } catch (e) {
       res.send('Error');
     }

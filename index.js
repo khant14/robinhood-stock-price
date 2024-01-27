9edeb5b1a5980276df346d833e115ac4da98950b
Aung Khant <khant@Aungs-MBP.lan>
2024-01-26 21:11:39 -0600

    crypto support

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 let browser;
 let pageMap = new Map();
 let priceMap = new Map();
-const PORT = process.env.PORT || 8001;
+const PORT = process.env.PORT || 8003;
 const BLOCKED_RESOURCES = ['image', 'stylesheet', 'ping', 'font'];
 
 (async () => {
@@ -15,13 +15,13 @@ const BLOCKED_RESOURCES = ['image', 'stylesheet', 'ping', 'font'];
     ]
   });
 
-  const createPage = async (ticker) => {
+  const createPage = async (ticker, type = 'stocks') => {
     const page = await browser.newPage();
     await page.setRequestInterception(true);
 
     // Block content for loading faster
     page.on("request", request => {
-      if (!request.url().includes('robinhood') || BLOCKED_RESOURCES.includes(request.resourceType())) {
+      if (BLOCKED_RESOURCES.includes(request.resourceType())) {
         request.abort();
       } else {
         request.continue();
@@ -30,28 +30,38 @@ const BLOCKED_RESOURCES = ['image', 'stylesheet', 'ping', 'font'];
     // Listen for stock price change
     page.on('response', async (response) => {
       try {
-        if (response.url().includes("marketdata/quotes")) {
+        if (response.url().includes('quotes')) {
           const stockQuotes = await response.json();
-          const symbol = stockQuotes.results[0].symbol;
-          priceMap.set(symbol, stockQuotes.results[0]);
+          const symbol = stockQuotes.results ? stockQuotes.results[0].symbol : stockQuotes.symbol;
+          priceMap.set(symbol, stockQuotes.results ? stockQuotes.results[0] : stockQuotes);
         }
       } catch (e) { }
     });
     // Set screen size
     await page.setViewport({ width: 100, height: 100 });
     // Navigate the page to a URL
-    await page.goto(`https://robinhood.com/us/en/stocks/${ticker}/`);
-    // Wait for page load
-    await page.waitForSelector('#sdp-market-price');
+    await page.goto(`https://robinhood.com/us/en/${type}/${ticker}/`, { waitUntil: 'networkidle0' });
     // Save page tab for future
     pageMap.set(ticker, page);
   }
 
-  app.get('/:ticker', async (req, res) => {
+  app.get('/stocks/:ticker', async (req, res) => {
     try {
       const ticker = req.params.ticker;
-      if (!pageMap.has(ticker)) await createPage(ticker);
+      if (!pageMap.has(ticker)) await createPage(ticker, 'stocks');
       const stockQuote = priceMap.get(ticker);
+
+      res.send(stockQuote);
+    } catch (e) {
+      res.send('Error');
+    }
+  });
+
+  app.get('/crypto/:ticker', async (req, res) => {
+    try {
+      const ticker = req.params.ticker;
+      if (!pageMap.has(ticker)) await createPage(ticker, 'crypto');
+      const stockQuote = priceMap.get(`${ticker}USD`);
 
       res.send(stockQuote);
     } catch (e) {
